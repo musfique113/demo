8c0157831332e3e4837b56a878999b6f38534f66
musfique113 <musfique113@gmail.com>
2024-05-02 19:44:04 +0600

    [Template] : Dio network executor, logger , interceptor, network response with success or failure entity

---
+++ lib/core/domain/data/model/api_success.dart
@@ -0,0 +1,19 @@
+import 'package:demo/core/domain/entites/success.dart';
+
+class ApiSuccess {
+  bool? success;
+  int? responseCode;
+  String? message;
+
+  ApiSuccess({this.success, this.responseCode, this.message});
+
+  ApiSuccess.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    responseCode = json['response_code'];
+    message = json['message'];
+  }
+
+  Success toEntity() {
+    return Success(message);
+  }
+}
